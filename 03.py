83a4536155825239a1e24351695ccb10de2ab7ce
Ajurna <ajurna@gmail.com>
2022-12-04 11:52:30 +0000

    day 3

--- 03.py
+++ 03.py
@@ -2,18 +2,13 @@ from aoc import get_input
 import string
 data = get_input(3).splitlines()
 
-# data = """vJrwpWtwJgWrhcsFMMfFFhFp
-# jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-# PmmdzqPrVvPwwTWBwg
-# wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-# ttgJtRGJQctTZtZT
-# CrZsJsPPZsGzwwsLwLmpwMDw""".splitlines()
 score_table = " "+string.ascii_lowercase+string.ascii_uppercase
 score = 0
 for line in data:
     ans = (set(line[:len(line)//2]) & set(line[len(line)//2:])).pop()
     score += score_table.index(ans)
-print(score)
+print(f"Part 1: {score}")
+
 score = 0
 while data:
     bag1 = set(data.pop())
@@ -21,4 +16,4 @@ while data:
     bag3 = set(data.pop())
     ans = bag1 & bag2 & bag3
     score += score_table.index(ans.pop())
-print(score)
+print(f"Part 2: {score}")
