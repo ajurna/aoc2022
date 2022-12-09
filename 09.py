724a224bfb37e1310824a44f9dba9be4a0495cd2
Peter Dwyer <peter.dwyer@clanwilliamhealth.com>
2022-12-09 15:07:00 +0000

    day 9

--- 09.py
+++ 09.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import NamedTuple
 
 from aoc import get_input
@@ -38,21 +39,48 @@ directions = {
 
 head = Point(0, 0)
 tail = Point(0, 0)
-tail_visited = set(tail)
+tail_visited = set()
+tail_visited2 = set()
+tail_visited.add(tail)
+tail_visited2.add(tail)
 data = get_input(9).splitlines()
-# data ="""U 1
-# R 2
-# D 2
-# L 2
-# R 2
-# U 2
-# """.splitlines()
+# data ="""R 5
+# U 8
+# L 8
+# D 3
+# R 17
+# D 10
+# L 25
+# U 20""".splitlines()
 
+
+@dataclass
+class Rope:
+    head: Point
+    def __post_init__(self):
+        self.body = [self.head] * 10
+
+    def move_body(self):
+        self.body[1] = self.head
+        for i in range(len(self.body) - 1):
+            head = self.body[i]
+            tail = self.body[i+1]
+            if head.distance(tail) > 1:
+                if head.x == tail.x and head.y != tail.y:
+                    tail = down(tail) if head.y < tail.y else up(tail)
+                elif head.x != tail.x and head.y == tail.y:
+                    tail = left(tail) if head.x < tail.x else right(tail)
+                else:
+                    tail = down(tail) if head.y < tail.y else up(tail)
+                    tail = left(tail) if head.x < tail.x else right(tail)
+                self.body[i+1] = tail
+        tail_visited2.add(self.body[-1])
+rope = Rope(head)
 for line in data:
-    d, dis = line.split()
-    dis = int(dis)
-    for _ in range(dis):
-        head: Point = directions[d](head)
+    direction, distance = line.split()
+    distance = int(distance)
+    for _ in range(distance):
+        head: Point = directions[direction](head)
         if head.distance(tail) > 1:
             if head.x == tail.x and head.y != tail.y:
                 tail = down(tail) if head.y < tail.y else up(tail)
@@ -62,4 +90,9 @@ for line in data:
                 tail = down(tail) if head.y < tail.y else up(tail)
                 tail = left(tail) if head.x < tail.x else right(tail)
             tail_visited.add(tail)
+        rope.head = directions[direction](head)
+        rope.move_body()
+
 print(len(tail_visited))
+print(len(tail_visited2))
+
