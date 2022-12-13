5159a4ff9c3cfb1bcee6a4270905fbe9481fc0d3
Ajurna <ajurna@gmail.com>
2022-12-13 10:36:20 +0000

    day 13

---
+++ 13.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from typing import List
+
+from aoc import get_input
+
+data = get_input(13).splitlines()
+
+
+def check_pair(p1, p2):
+    for i1, i2 in zip(p1, p2):
+        if isinstance(i1, int) and isinstance(i2, int):
+            if i1 < i2:
+                return 'in_order'
+            elif i1 > i2:
+                return 'out_of_order'
+        elif isinstance(i1, list) and isinstance(i2, list):
+            result = check_pair(i1, i2)
+            if result in ['in_order', 'out_of_order']:
+                return result
+            if len(i1) < len(i2):
+                return 'in_order'
+            if len(i1) > len(i2):
+                return 'out_of_order'
+        elif isinstance(i1, list) and isinstance(i2, int):
+            result = check_pair(i1, [i2])
+            if result in ['in_order', 'out_of_order']:
+                return result
+        elif isinstance(i1, int) and isinstance(i2, list):
+            result = check_pair([i1], i2)
+            if result in ['in_order', 'out_of_order']:
+                return result
+    if len(p1) < len(p2):
+        return 'in_order'
+    if len(p1) > len(p2):
+        return 'out_of_order'
+    return 'match'
+
+
+def lt(p1, p2):
+    return check_pair(p1, p2) == "in_order"
+
+
+@dataclass
+class Message:
+    data: List
+
+    def __lt__(self, other):
+        return check_pair(self.data, other.data) == "in_order"
+
+
+all_items = []
+part1 = 0
+index = 1
+while data:
+    pair1 = eval(data.pop(0))
+    pair2 = eval(data.pop(0))
+    all_items.append(Message(pair1))
+    all_items.append(Message(pair2))
+    if len(data) > 0 and data[0] == "":
+        data.pop(0)
+    result = check_pair(pair1, pair2)
+    if result == "in_order":
+        part1 += index
+    index += 1
+print("Part 1:", part1)
+
+div_1 = Message([[2]])
+div_2 = Message([[6]])
+all_items.append(div_1)
+all_items.append(div_2)
+
+all_items.sort()
+print("Part 2:", (all_items.index(div_1)+1)*(all_items.index(div_2)+1))
